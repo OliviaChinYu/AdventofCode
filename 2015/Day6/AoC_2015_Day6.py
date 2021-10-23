2a8fb52f607b039d80e73fbb2b941fff3676439f
Olivia Chin <olivia.chin@mindbodyonline.com>
2021-10-22 21:21:49 -0700

    Ints

--- 2015/Day6/AoC_2015_Day6.py
+++ 2015/Day6/AoC_2015_Day6.py
@@ -8,29 +8,33 @@ __location__ = os.path.realpath(
 f = open(os.path.join(__location__, 'input.txt'), 'r')
 
 lst = []
-for i in range(1000):
-	lst.append(-1)
+for i in range(15):
+	lst.append(0)
 
 df = pd.DataFrame()
 inc = 0
-for i in range(1000):
+
+for i in range(15):
 	df[inc] = np.array(lst)
 	inc = inc + 1
 
+
 for line in f:
 	instruc = line.replace(' ',',')
+	instruc = instruc.replace('\n','')
 	instr_list = instruc.split(',')
+	print(instr_list)
+	row_start = int(instr_list[-5]) # min x height
+	row_end = int(instr_list[-2]) # max x height
+	col_start = int(instr_list[-4]) # min y w
+	col_end = int(instr_list[-1]) # max y w
 	if 'on' in instr_list:
-		df.loc[instr_list[-4]:instr_list[-1], instr_list[-5]:instr_list[-2]] = 1
+		df.loc[row_start:row_end, col_start:col_end] = 1
 	elif 'off' in instr_list:
-		df.loc[instr_list[-4]:instr_list[-1], instr_list[-5]:instr_list[-2]] = -1
+		df.loc[row_start:row_end, col_start:col_end] = 0
 	elif 'toggle' in instr_list:
-		df.loc[instr_list[-4]:instr_list[-1], instr_list[-5]:instr_list[-2]] = df.loc[instr_list[-4]:instr_list[-1], instr_list[-5]:instr_list[-2]]*-1	
+		df.loc[row_start:row_end, col_start:col_end] = df.loc[row_start:row_end, col_start:col_end] == 0 if 1 else 0	
 	else:
-		sys.exit()
-
-print(df)
-print(df.values.sum())
-df = df.mask(df < 0, 0)
+		pass
 
 print(df.values.sum())
